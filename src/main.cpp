0955d877e7e761a74b9e830f65c208c9dc768053
Davide <dvd.rori879@gmail.com>
2022-05-12 20:20:24 +0200

    Added color sensor

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,12 @@
 #include "util.h"
 #include "Sensors/Gyro.h"
 #include "Sensors/Laser.h"
+#include "Sensors/Color.h"
 
 DynamicJsonDocument doc(1024);
 String res = "";
+
+tcs34725 rgb_sensor;
 
 Laser *l0, *l1;
 Gyro *mpu;
@@ -33,6 +36,8 @@ void setup()
 		SignalBuiltinLED(2, 50);
 	;
 
+	rgb_sensor.begin();
+
 #if DEBUG == true
 	mpu = new Gyro(LED_0);
 	l0 = new Laser(&Wire, LED_1);
@@ -42,7 +47,58 @@ void setup()
 	l0 = new Laser(&Wire);
 	l1 = new Laser(&Wire2);
 #endif
+}
 
+void printColorSensor()
+{
+	Serial.print(F("Gain:"));
+	Serial.print(rgb_sensor.againx);
+	Serial.print(F("x "));
+	Serial.print(F("Time:"));
+	Serial.print(rgb_sensor.atime_ms);
+	Serial.print(F("ms (0x"));
+	Serial.print(rgb_sensor.atime, HEX);
+	Serial.println(F(")"));
+
+	Serial.print(F("Raw R:"));
+	Serial.print(rgb_sensor.r);
+	Serial.print(F(" G:"));
+	Serial.print(rgb_sensor.g);
+	Serial.print(F(" B:"));
+	Serial.print(rgb_sensor.b);
+	Serial.print(F(" C:"));
+	Serial.println(rgb_sensor.c);
+
+	Serial.print(F("IR:"));
+	Serial.print(rgb_sensor.ir);
+	Serial.print(F(" CRATIO:"));
+	Serial.print(rgb_sensor.cratio);
+	Serial.print(F(" Sat:"));
+	Serial.print(rgb_sensor.saturation);
+	Serial.print(F(" Sat75:"));
+	Serial.print(rgb_sensor.saturation75);
+	Serial.print(F(" "));
+	Serial.println(rgb_sensor.isSaturated ? "*SATURATED*" : "");
+
+	Serial.print(F("CPL:"));
+	Serial.print(rgb_sensor.cpl);
+	Serial.print(F(" Max lux:"));
+	Serial.println(rgb_sensor.maxlux);
+
+	Serial.print(F("Compensated R:"));
+	Serial.print(rgb_sensor.r_comp);
+	Serial.print(F(" G:"));
+	Serial.print(rgb_sensor.g_comp);
+	Serial.print(F(" B:"));
+	Serial.print(rgb_sensor.b_comp);
+	Serial.print(F(" C:"));
+	Serial.println(rgb_sensor.c_comp);
+
+	Serial.print(F("Lux:"));
+	Serial.print(rgb_sensor.lux);
+	Serial.print(F(" CT:"));
+	Serial.print(rgb_sensor.ct);
+	Serial.println(F("K"));
 }
 
 void loop()
@@ -50,6 +106,11 @@ void loop()
 	gyro = mpu->GetGyroData();
 	laser0_data = l0->GetData();
 	laser1_data = l1->GetData();
+	START_TIMER
+	rgb_sensor.getData();
+	END_TIMER
+
+	printColorSensor();
 
 #if DEBUG == true
 	doc[DATANAME_GYRO_X] = gyro.x;
