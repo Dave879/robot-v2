6b6be47b6648b3df4e74467c445c36b20d55d598
Dave879 <dvd.rori879@gmail.com>
2023-05-03 18:39:40 +0200

    Added data formatter

--- include/data_formatter.h
+++ include/data_formatter.h
@@ -5,16 +5,35 @@
 #include <string>
 #include <iomanip>
 
+#define PRINT(x)   Serial.print("{\"0:s\":\""); Serial.print(x);   Serial.print("\"}");
+#define PRINTLN(x)   Serial.print("{\"0:s\":\""); Serial.print(x);   Serial.print("\\n\"}");
+
+enum color
+{
+	RED = 'r',
+	GREEN = 'g',
+	BLUE = 'b'
+};
+
 class DataFormatter
 {
 private:
 	uint16_t graph_index_;
+	uint16_t heatmap_index_;
 	std::stringstream tmp_ss_;
+	uint64_t pkt_idx_;
+	void CleanStreamAndIncrementIndex(uint16_t &idx);
+
 public:
 	DataFormatter();
 	std::string AddBarGraph(std::string name);
+	std::string AddStringLog(color c);
+	std::string AddStringLog();
+	std::string AddPacketIndex();
 	std::string AddLineGraph(std::string name);
-	std::string AddHeatmap(std::string name, uint8_t sizex, uint8_t sizey);
+	std::string AddLineGraph(std::string name, int64_t min, int64_t max);
+	std::string AddHeatmap(std::string name, uint16_t sizex, uint16_t sizey, int32_t min, int32_t max);
+	uint64_t GetPacketIdx();
 	void ResetIdx();
 	~DataFormatter();
 };
--- src/data_formatter.cpp
+++ src/data_formatter.cpp
@@ -1,33 +1,72 @@
 #include "data_formatter.h"
 
-DataFormatter::DataFormatter() : graph_index_(0)
+DataFormatter::DataFormatter() : graph_index_(0), heatmap_index_(0), pkt_idx_(0)
 {
 }
 
 std::string DataFormatter::AddBarGraph(std::string name)
 {
+	CleanStreamAndIncrementIndex(graph_index_);
+	tmp_ss_ << "n:b:" << name;
+	return tmp_ss_.str();
+}
+
+std::string DataFormatter::AddStringLog(color c)
+{
 	tmp_ss_.str("");
-	tmp_ss_ << std::setfill('0') << std::setw(sizeof(uint16_t) * 2) << std::hex << graph_index_++ << "nb>" << name;
+	tmp_ss_ << "0:s:" << c;
+	return tmp_ss_.str();
+}
+
+std::string DataFormatter::AddStringLog()
+{
+	tmp_ss_.str("");
+	tmp_ss_ << "0:s";
+	return tmp_ss_.str();
+}
+
+std::string DataFormatter::AddPacketIndex()
+{
+	tmp_ss_.str("");
+	tmp_ss_ << "0:i";
 	return tmp_ss_.str();
 }
 
 std::string DataFormatter::AddLineGraph(std::string name)
 {
-	tmp_ss_.str("");
-	tmp_ss_ << std::setfill('0') << std::setw(sizeof(uint16_t) * 2) << std::hex << graph_index_++ << "nl>" << name;
+	CleanStreamAndIncrementIndex(graph_index_);
+	tmp_ss_ << "n:l:" << name;
 	return tmp_ss_.str();
 }
 
-std::string DataFormatter::AddHeatmap(std::string name, uint8_t sizex, uint8_t sizey)
+std::string DataFormatter::AddLineGraph(std::string name, int64_t min, int64_t max)
+{
+	CleanStreamAndIncrementIndex(graph_index_);
+	tmp_ss_ << "n:l:" << name << ":" << min << ":" << max;
+	return tmp_ss_.str();
+}
+
+std::string DataFormatter::AddHeatmap(std::string name, uint16_t sizex, uint16_t sizey, int32_t min, int32_t max)
+{
+	CleanStreamAndIncrementIndex(heatmap_index_);
+	tmp_ss_ << "n:h:" << name << ":" << sizex << ":" << sizey << ":" << min << ":" << max;
+	return tmp_ss_.str();
+}
+
+void DataFormatter::CleanStreamAndIncrementIndex(uint16_t &idx)
 {
 	tmp_ss_.str("");
-	tmp_ss_ << std::setfill('0') << std::setw(sizeof(uint16_t) * 2) << std::hex << graph_index_++ << "nh" << sizex << "*" << sizey << ">" << name;
-	return tmp_ss_.str();
+	tmp_ss_ << idx++ << ":";
+}
+
+uint64_t DataFormatter::GetPacketIdx(){
+	return pkt_idx_++;
 }
 
 void DataFormatter::ResetIdx()
 {
 	graph_index_ = 0;
+	heatmap_index_ = 0;
 }
 
 DataFormatter::~DataFormatter()
